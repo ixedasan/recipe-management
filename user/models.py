cd5683158919b6e07d4320e0ded79c804195fdd9
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-04-11 19:58:36 +0300

    structure template components

--- user/models.py
+++ user/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image_profile = models.ImageField(upload_to='Users/Profile_images', null=True, blank=True)
-                                      # default='Users/Profile_images/default.jpg')
+    # default='Users/Profile_images/default.jpg')
     bio = models.TextField(null=True, blank=True)
+
     def __str__(self):
         return self.user.username
