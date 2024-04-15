1ba16af0695d089d4598f356ec461c39774c4c9e
Yatsyshyn Oleksandr <pandikgenshin@gmail.com>
2024-04-15 21:40:43 +0300

    developing home page

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from recipes.models import Category, Recipe
 
-# Create your views here.
+
+def index(request):
+    recipes = Recipe.objects.all().order_by("-id")[:6]
+    categories = Category.objects.all().order_by("?")[:5]
+    context = {
+        'recipes': recipes,
+        'categories': categories,
+    }
+    return render(request, 'index.html', context)
